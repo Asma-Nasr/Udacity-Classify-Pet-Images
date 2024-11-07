e1ececb38762c041e1280382fdd52ad7efd36e52
Asmaa Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2024-11-07 20:07:10 +0200

    Update print_results.py

--- print_results.py
+++ print_results.py
@@ -138,6 +138,6 @@ def print_results(results_dic, results_stats_dic, model,
             # Pet Image Label is-a-Dog, classified as-a-dog but is WRONG breed
             if ( sum(results_dic[key][3:]) == 2 and
                 results_dic[key][2] == 0 ):
-                print("Real: {:>26}   Classifier: {:>30}".format(results_dic[key][0],
+                print("Real: {:>30}   Classifier: {:>30}".format(results_dic[key][0],
                                                           results_dic[key][1]))
                 
